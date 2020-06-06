58995ed2f03fee0ac113ef88ca8a6973ec453e1b
Phi2212 <66456619+Phi2212@users.noreply.github.com>
2020-06-06 11:07:47 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'ec2-184-73-209-230.compute-1.amazonaws.com');
-define('DB_USERNAME', 'mtycxjjccxmeee');
-define('DB_PASSWORD', 'f0dd25aaa62a3a7a83d3934915c681f6ddfac747814e5a3668945ea34017c6ce');
-define('DB_NAME', 'd35915da8p1v65');
+define('DB_SERVER', 'ec2-52-70-15-120.compute-1.amazonaws.com');
+define('DB_USERNAME', 'wkvuncwgqlgdqd');
+define('DB_PASSWORD', '742b848c49a358e4d449b68af070c7aec89ecaa93d30f294dcf8372802181599');
+define('DB_NAME', 'drjusr443eib0');
  
 /* Attempt to connect to PostgreSQL database */
 $link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
